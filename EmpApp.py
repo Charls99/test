f717b562d8a7b38a16cf869e5080c785d7e671cf
Charls99 <wenghoong99@yahoo.com>
2022-09-21 14:35:39 +0800

    new

--- EmpApp.py
+++ EmpApp.py
@@ -23,7 +23,7 @@ table = 'employee'
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template('Attendance.html')
+    return render_template('index.html')
 
 
 @app.route("/about", methods=['POST'])
