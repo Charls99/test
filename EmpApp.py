36c75c578ff57eb9aad938d683ff917c0f39a65b
Kahyee18 <kahyee57@gmail.com>
2022-09-26 20:37:06 +0800

    update profile

--- EmpApp.py
+++ EmpApp.py
@@ -37,7 +37,8 @@ def profile():
     profile_image_staff_1 = "https://yvonne-test.s3.amazonaws.com/{0}".format('staff_1.jpg')
     profile_image_staff_2 = "https://yvonne-test.s3.amazonaws.com/{0}".format('staff_2.jpg')
     profile_image_staff_3 = "https://yvonne-test.s3.amazonaws.com/{0}".format('staff_3.jpg')
-    return render_template("Profile.html", profile_image_staff_1,profile_image_staff_2,profile_image_staff_3 )
+    profile = [profile_image_staff_1,profile_image_staff_2,profile_image_staff_3]
+    return render_template("Profile.html", profile)
 
 
 @app.route("/Employees")
