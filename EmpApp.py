17769d3a5f0508676775eea82cb4a0fd059d76a4
Kahyee18 <kahyee57@gmail.com>
2022-09-26 20:29:06 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -34,7 +34,7 @@ def index():
 def profile():
     bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
     s3_location = (bucket_location['LocationConstraint'])
-    profile_image_staff_1 = "https://{0}.s3.amazonaws.com/{2}".format(custombucket,'staff_2.jpg')
+    profile_image_staff_1 = "https://yvonne-test.s3.amazonaws.com/{0}".format('staff_2.jpg')
     return render_template("Profile.html", user_image=profile_image_staff_1)
 
 
