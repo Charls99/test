6374e314fa40f02e1edcb9c941c59e74bb06fbf7
Kahyee18 <kahyee57@gmail.com>
2022-09-26 20:33:44 +0800

    update employee

--- EmpApp.py
+++ EmpApp.py
@@ -34,8 +34,10 @@ def index():
 def profile():
     bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
     s3_location = (bucket_location['LocationConstraint'])
-    profile_image_staff_1 = "https://yvonne-test.s3.amazonaws.com/{0}".format('staff_2.jpg')
-    return render_template("Profile.html", user_image=profile_image_staff_1)
+    profile_image_staff_1 = "https://yvonne-test.s3.amazonaws.com/{0}".format('staff_1.jpg')
+    profile_image_staff_2 = "https://yvonne-test.s3.amazonaws.com/{0}".format('staff_2.jpg')
+    profile_image_staff_3 = "https://yvonne-test.s3.amazonaws.com/{0}".format('staff_3.jpg')
+    return render_template("Profile.html", profile_image_staff_1,profile_image_staff_2,profile_image_staff_3 )
 
 
 @app.route("/Employees")
