2379923bdcdae1554bdf8118cca2bafadb1dc4ec
Charls99 <wenghoong99@yahoo.com>
2022-09-27 23:16:20 +0800

    profile

--- Database.py
+++ Database.py
@@ -1,25 +1,3 @@
-CREATE TABLE `employee` (
-	`eid` INT(10) NOT NULL AUTO_INCREMENT,
-	`fname` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	`lname` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	`dept` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	`deg` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	`role` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	`gender` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	`blood` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	`nid` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	`contact` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	`dob` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	`joindate` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	`leavedate` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	`username` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	`email` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
-	PRIMARY KEY (`eid`) USING BTREE
-)
-COLLATE='utf8mb4_0900_ai_ci'
-ENGINE=InnoDB
-;
-
 CREATE TABLE `attendance` (
 	`id` INT(10) NOT NULL AUTO_INCREMENT,
 	`emid` VARCHAR(64) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
@@ -31,91 +9,63 @@ CREATE TABLE `attendance` (
 )
 COLLATE='utf8mb4_0900_ai_ci'
 ENGINE=InnoDB
-AUTO_INCREMENT=4
 ;
 
-CREATE TABLE `leave` (
-  `id` int(11) NOT NULL,
-  `em_id` varchar(64) DEFAULT NULL,
-  `leave_type` varchar(64) DEFAULT NULL,
-  `start_date` varchar(64) DEFAULT NULL,
-  `end_date` varchar(64) DEFAULT NULL,
-  `reason` varchar(1024) DEFAULT NULL
-) ENGINE=InnoDB;
-
-CREATE TABLE `salary` (
-	`sid` INT(10) NOT NULL AUTO_INCREMENT,
-	`typeid` VARCHAR(50) NULL DEFAULT NULL,
-	`total` DOUBLE NULL DEFAULT NULL,
-	`basic` DOUBLE NULL DEFAULT NULL,
-	`houserent` DOUBLE NULL DEFAULT NULL,
-	`medical` DOUBLE NULL DEFAULT NULL,
-	`conveyance` DOUBLE NULL DEFAULT NULL,
-	`emid` VARCHAR(50) NULL DEFAULT NULL
+CREATE TABLE `empLeave` (
+	`id` INT(10) NOT NULL AUTO_INCREMENT,
+	`em_id` VARCHAR(64) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`leave_type` VARCHAR(64) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`start_date` VARCHAR(64) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`end_date` VARCHAR(64) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`leaveStatus` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`reason` VARCHAR(255) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	PRIMARY KEY (`id`) USING BTREE
 )
 COLLATE='utf8mb4_0900_ai_ci'
 ENGINE=InnoDB
 ;
 
+CREATE TABLE `employee` (
+	`fname` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`lname` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`eid` INT(10) NULL DEFAULT NULL,
+	`dept` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`deg` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`role` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`gender` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`blood` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`nid` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`contact` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`dob` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`joindate` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`leavedate` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`username` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`email` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci'
+)
+COLLATE='utf8mb4_0900_ai_ci'
+ENGINE=InnoDB
+;
 
+CREATE TABLE `paySalary` (
+	`pay_id` INT(10) NOT NULL AUTO_INCREMENT,
+	`emid` INT(10) NULL DEFAULT NULL,
+	`month` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`total_paid` DOUBLE NULL DEFAULT NULL,
+	`status` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`paidType` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	PRIMARY KEY (`pay_id`) USING BTREE
+)
+COLLATE='utf8mb4_0900_ai_ci'
+ENGINE=InnoDB
+;
 
-@app.route("/AddEmpData", methods=['POST'])
-def addEmployeeData():
-#add userdata when press submit button#
-    fname = request.form['fname']
-    lname = request.form['lname']
-    eid = request.form['eid']
-    dept = request.form['dept']
-    deg = request.form['deg']
-    role = request.form['role']
-    gender = request.form['gender']
-    blood = request.form['blood']
-    nid = request.form['nid']
-    contact = request.form['contact']
-    dob = request.form['dob']
-    joindate = request.form['joindate']
-    leavedate = request.form['leavedate']
-    username = request.form['username']
-    email = request.form['email']
-    image_url = request.files['image_url']
-
-    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
-    cursor = db_conn.cursor()
-
-    if image_url.filename == "":
-        return "Please select a file"
-
-    try:
-
-        cursor.execute(insert_sql, (fname, lname, eid, dept, deg, role, gender, blood, nid, contact, dob, joindate, leavedate, username, email))
-        db_conn.commit()
-        #Set name for listout#
-        emp_name = "" + fname + " " + lname
-        # Uplaod image file in S3 #
-        emp_image_file_name_in_s3 = "emp-id-" + str(emp_name) + "_image_file"
-        s3 = boto3.resource('s3')
-
-        try:
-            print("Data inserted in MySQL RDS... uploading image to S3...")
-            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=image_url)
-            bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
-            s3_location = (bucket_location['LocationConstraint'])
-
-            if s3_location is None:
-                s3_location = ''
-            else:
-                s3_location = '-' + s3_location
-
-            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
-                s3_location,
-                custombucket,
-                emp_image_file_name_in_s3)
-
-        except Exception as e:
-            return str(e)
-
-    finally:
-        cursor.close()
-
-    print("all modification done...")
-    return render_template('Add_employee.html')
+CREATE TABLE `salary` (
+	`sid` INT(10) NOT NULL AUTO_INCREMENT,
+	`emid` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`typeid` VARCHAR(50) NULL DEFAULT NULL COLLATE 'utf8mb4_0900_ai_ci',
+	`total` DOUBLE NULL DEFAULT NULL,
+	PRIMARY KEY (`sid`) USING BTREE
+)
+COLLATE='utf8mb4_0900_ai_ci'
+ENGINE=InnoDB
+;
