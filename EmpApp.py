61cd1a0dd79f41a6624b11d2725cfdb6f838f166
Charls99 <wenghoong99@yahoo.com>
2022-09-28 10:13:49 +0800

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -103,7 +103,7 @@ def addEmployee():
 
         try:
             print("Data inserted in MySQL RDS... uploading image to S3...")
-            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=image_url.read(), Metadata=metadata, MetadataDirective='REPLACE')
+            s3.Bucket(custombucket).put_object(Key=emp_image_file_name_in_s3, Body=image_url.read(), Metadata=metadata)
             bucket_location = boto3.client('s3').get_bucket_location(Bucket=custombucket)
             s3_location = (bucket_location['LocationConstraint'])
 
